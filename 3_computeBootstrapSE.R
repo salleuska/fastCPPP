3d8067c5ca7f781ce8586ec8a464e662855da8a0
salleuska <sallypaganin@gmail.com>
2022-12-09 14:13:04 -0500

    added boostrap estimator comp

--- 3_computeBootstrapSE.R
+++ 3_computeBootstrapSE.R
@@ -12,6 +12,10 @@ args <- R.utils::commandArgs(asValue=TRUE)
 ## --indexStat		## index for the discrepancy to use (the runCalibration() function allows for multiple discrepancies)
 ## --bootIters      ## numer of bootstrap replications
 #######################
+# args <- list()
+# args$filename="newcomb/results_nCRep_1000_nIter_1000.rds" 
+# args$indexStat=2 
+# args$bootIters=100
 
 ## number of times we want to repeat the computation
 N <- 100
@@ -43,7 +47,7 @@ M <- c(50, 100, 200, 500)
 rTOT <- length(res$repDisc)
 
 ###############################
-## Variance trasfer method
+## variance estimate via bootstrap
 ###############################
 ## Compute differences between discrepancy values at each iteration
 deltaObs <- res$obsDisc[indexStat,,2] - res$obsDisc[indexStat,,1]
@@ -53,6 +57,9 @@ mOrig <- length(deltaObs)
 varianceMBB <- array(0, dim = c(length(compCost), length(M)))
 varianceNormal <- array(0, dim = c(length(compCost), length(M)))
 
+averageCoverageMBB <- array(0, dim = c(length(compCost), length(M)))
+averageCoverageNormal <- array(0, dim = c(length(compCost), length(M)))
+
 mcVarMBB <- array(0, dim = c(length(compCost), length(M)))
 mcVarNormal <- array(0, dim = c(length(compCost), length(M)))
 
@@ -61,106 +68,119 @@ mcVarNormal <- array(0, dim = c(length(compCost), length(M)))
 tmpVarMBB <- numeric(N)
 tmpNormal <- numeric(N)
 
+coverageMBB <- numeric(N)
+coverageNormal <- numeric(N)
+
+
+cpppBootNormal <- numeric(bootIters)
+cpppBootMBB    <- numeric(bootIters)
+
 obsPPP <- res$obsPPP[indexStat]
 
-# cat("Start boostrap variance estimates \n")
+cat("Start boostrap variance estimates \n")
 
-# for(r in 1:length(compCost)){	
-# 	R <- compCost[r]/M
-# 	for(m in 1:length(M)){
-# 	cat(paste0("comp cost = ", compCost[c], " MCMCsamples = ", M[m], " Cal. Rep = ", R[m], " \n"))
-# 		for(k in 1:N){
+# r <- m <- k <- 1
+for(r in 1:length(compCost)){	
+	R <- compCost[r]/M
+	for(m in 1:length(M)){
+	cat(paste0("comp cost = ", compCost[r], " MCMCsamples = ", M[m], " Cal. Rep = ", R[m], " \n"))
+		for(k in 1:N){
 
-# 			discList <- res$repDisc[sample(1:rTOT, R[m], replace = F)]
+			discList <- res$repDisc[sample(1:rTOT, R[m], replace = F)]
 
-# 			cpppBootApprox <- numeric(bootIters)
-# 			cpppBootApproxQuantile <- numeric(bootIters)
+			for(j in 1:bootIters){
 
-# 			cpppBootMBB    <- numeric(bootIters)
-# 			for(j in 1:bootIters){
+				indexBoot <- sample(1:R[m], R[m], replace = T)
 
-# 				indexBoot <- sample(1:R[m], R[m], replace = T)
-
-# 				bootDiscList <- discList[indexBoot]
-# 				## use "continuity correction"
-# 				pppVec <- unlist(lapply(bootDiscList, function(x) 
-# 					(sum(x[indexStat,1:M[m],2] >= x[indexStat,1:M[m],1]) + 0.5)/(M[m] + 1)))
+				bootDiscList <- discList[indexBoot]
+				## use "continuity correction"
+				pppVec <- unlist(lapply(bootDiscList, function(x) 
+					(sum(x[indexStat,1:M[m],2] >= x[indexStat,1:M[m],1]) + 0.5)/(M[m] + 1)))
 
 
-# 				## 1) normal approximation - sample kVec ~ N(pppVec, sqrt(varApprox))
-# 				tauTransfer <- numeric(R[m])
-# 				deltaQ 		<- quantile(deltaObs, prob = pppVec)
+				## 1) normal approximation - sample kVec ~ N(pppVec, sqrt(varApprox))
+				tauTransfer <- numeric(R[m])
+				deltaQ 		<- quantile(deltaObs, prob = pppVec)
 
-# 				for(i in 1:R[m]){
-# 		            indSeqTransfer 		<- as.numeric(I(deltaObs <= deltaQ[i]))
-# 					if(length(unique(indSeqTransfer)) == 1) {
-# 						## If there is no variance, tau is enormous
-# 						tauTransfer[i] <- 1e+10
-# 					} else {
-# 						varianceMCMC 	<- try(mcse(indSeqTransfer, r = 3)$se^2, silent = TRUE)
+				for(i in 1:R[m]){
+		            indSeqTransfer 		<- as.numeric(I(deltaObs <= deltaQ[i]))
+					if(length(unique(indSeqTransfer)) == 1) {
+						## If there is no variance, tau is enormous
+						tauTransfer[i] <- 1e+10
+					} else {
+						varianceMCMC 	<- try(mcse(indSeqTransfer, r = 3)$se^2, silent = TRUE)
 						
-# 						varianceInd 	<- var(indSeqTransfer)
-# 						tauTransfer[i] 	<- (varianceMCMC * length(deltaObs))/varianceInd
-# 					}
-# 				}
+						varianceInd 	<- var(indSeqTransfer)
+						tauTransfer[i] 	<- (varianceMCMC * length(deltaObs))/varianceInd
+					}
+				}
 
-# 				# ## normal approximation for K
-# 				# meanVec <- M[m]*pppVec
-# 				# varVec <- M[m]*pppVec*(1 - pppVec)*tauTransfer
+				# ## normal approximation for K
+				meanVec <- M[m]*pppVec
+				varVec <- M[m]*pppVec*(1 - pppVec)*tauTransfer
 
-
-# 				meanVec <- M[m]*pppVec
-# 				varVec <- M[m]*pppVec*(1 - pppVec)*tauTransfer
-
-# 				kappaBoot <- rnorm(R[m], mean = M[m]*pppVec, sd = sqrt(varVec))
-# 				cpppBootApprox[j] <- mean(kappaBoot <= M[m]* obsPPP)
-
-# 				kappaBootProbs <- pnorm(M[m]*obsPPP, mean = meanVec, sd = sqrt(varVec))				
-# 				cpppBootApproxQuantile[j] <- mean(kappaBootProbs)
+				kappaBoot <- rnorm(R[m], mean = M[m]*pppVec, sd = sqrt(varVec))
+				cpppBootNormal[j] <- mean(kappaBoot <= M[m]* obsPPP)
 				
-# 				## 2) moving block bootstrap
+				## 2) moving block bootstrap
 
-# 				mbbIndex <- boot.mblock(M[m], b = max(2, round(M[m]/10)))
-# 				mbbPppVec <- unlist(lapply(bootDiscList, function(x) 
-# 					(sum(x[indexStat,mbbIndex,2] >= x[indexStat,mbbIndex,1]) + 0.5)/(M[m] + 1)))
+				mbbIndex <- boot.mblock(M[m], b = max(2, round(M[m]/10)))
+				mbbPppVec <- unlist(lapply(bootDiscList, function(x) 
+					(sum(x[indexStat,mbbIndex,2] >= x[indexStat,mbbIndex,1]) + 0.5)/(M[m] + 1)))
 
-# 				cpppBootMBB[j] <- mean(mbbPppVec <= obsPPP)
+				cpppBootMBB[j] <- mean(mbbPppVec <= obsPPP)
 
-# 				if(j %% 10 == 0) cat("boot iter - ", j, "\n")
-# 			}
+				if(j %% 10 == 0) cat("boot iter - ", j, "\n")
+			}
 
-# 			tmpVarMBB[k] <- var(cpppBootMBB)
-# 			tmpNormal[k] <- var(cpppBootApprox)
-
-# 		}
-
-# 		varianceMBB[r,m] <- mean(tmpVarMBB)
-# 		varianceNormal[r,m] <- mean(tmpNormal)
-
-# 		mcVarMBB[r,m] <- var(tmpVarMBB)
-# 		mcVarNormal[r,m] <- var(tmpNormal)
-
-# 	}
-# }
-
-# cat("End estimation boostrap variance \n")
-
-# dimnamesvarianceMBB) <- list(compCost, M)
-# dimnames(varianceNormal) <- list(compCost, M)
-# dimnames(mcVarMBB) <- list(compCost, M)
-# dimnames(mcVarNormal) <- list(compCost, M)
+			tmpVarMBB[k] <- var(cpppBootMBB)
+			tmpNormal[k] <- var(cpppBootNormal)
 
 
-# resBootVariance <- list(cpppEst = cpppEst, 
-# 						estimate = estimate, 
-# 						varianceMBB =varianceMBB, 
-# 						mcVarMBB = mcVarMBB,
-# 						varianceNormal = varianceNormal, 
-# 						mcVarNormal = mcVarNormal)
+			CILowMBB <- quantile(cpppBootMBB, 0.025)
+			CIUpBB <- quantile(cpppBootMBB, 0.975)
 
-# cat("Saving boostrap variance estimates \n")
+			coverageMBB[k] <- cpppEst <= CIUpBB & cpppEst >= CILowMBB
 
-# saveRDS(resBootVariance, file = paste0(dirExample, "/varianceBootstrap.rds" ))
+			CILowNormal <- quantile(cpppBootNormal, 0.025)
+			CIUpNormal <- quantile(cpppBootNormal, 0.975)
+
+			coverageNormal[k] <- cpppEst <= CIUpNormal & cpppEst >= CILowNormal
+
+
+		}
+
+		varianceMBB[r,m] <- mean(tmpVarMBB)
+		varianceNormal[r,m] <- mean(tmpNormal)
+
+		mcVarMBB[r,m] <- var(tmpVarMBB)
+		mcVarNormal[r,m] <- var(tmpNormal)
+
+		averageCoverageNormal[c, m] <- mean(coverageNormal)
+		averageCoverageMBB[c, m] <- mean(coverageMBB)
+
+	}
+}
+
+cat("End estimation boostrap variance \n")
+
+dimnames(varianceMBB) <- list(compCost, M)
+dimnames(varianceNormal) <- list(compCost, M)
+dimnames(mcVarMBB) <- list(compCost, M)
+dimnames(mcVarNormal) <- list(compCost, M)
+
+
+resBootVariance <- list(cpppEst = cpppEst, 
+						varianceMBB =varianceMBB, 
+						mcVarMBB = mcVarMBB,
+						varianceNormal = varianceNormal, 
+						mcVarNormal = mcVarNormal, 
+						averageCoverageMBB = averageCoverageMBB, 
+						averageCoverageNormal = averageCoverageNormal)
+
+cat("Saving boostrap variance estimates \n")
+
+saveRDS(resBootVariance, file = paste0(dirExample, "/varianceBootstrap.rds" ))
 
 
 
