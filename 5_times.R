f35745e17034c78af4bfbe721e2d4e70f54bf0ec
Paganin <paganin.1@osu.edu>
2024-01-19 19:19:40 -0500

    update times script

--- 5_times.R
+++ 5_times.R
@@ -27,4 +27,42 @@ dfTime[4, ]$minutes <- readRDS("sec6_examples/newcomb/results_nCRep_1000_nIter_1
 dfTime[5, ]$example <- "newcombBadMixing"
 dfTime[5, ]$minutes <- readRDS("sec6_examples/newcombBadMixing/results_nCRep_1000_nIter_1000.rds")$time[3]/60
 
+
 dfTime
+
+## dfTime contain the time to perform 1000 calibration replicates, each with 1000 MCMC samples
+
+dfTime$seconds <- dfTime$minutes * 60
+dfTime$mcmc1000 <- dfTime$seconds/1000
+dfTime$mcmc1 <- dfTime$mcmc1000/1000
+
+dfTime$ppp <- dfTime$mcmc1* c(10000, 10000, 10000, 5000, 5000)
+
+dfTime$c5000 <- dfTime$mcmc1* 5000
+dfTime$c10000 <- dfTime$mcmc1* 10000
+dfTime$c20000 <- dfTime$mcmc1* 20000
+dfTime$c50000 <- dfTime$mcmc1* 50000
+dfTime$naiveTime <- dfTime$ppp * 1000
+
+dfTime <- dfTime[c(4,5,2,1,3), ]
+
+dfTime[, c(6:11)]/60
+
+
+###################
+df <- data.frame(example = rep(dfTime$example, 4),
+ m = rep(c(50, 100, 200, 500), each = 5), 
+ mcmc1 = rep(dfTime$mcmc1, 4), 
+ naiveTime = rep(dfTime$naiveTime, 4))
+
+df$time = df$m * df$mcmc1 * 1000
+df$saving = df$naiveTime/df$time
+
+ggplot(data = df, aes(x = m, y = time/60, color = example)) + 
+geom_point() + geom_line() + theme_bw() + 
+xlab(TeX("\\tilde{m}")) + ylab("time in minutes") 
+
+
+ggplot(data = df, aes(x = m, y = naiveTime/60, color = example)) + 
+geom_point() + geom_line() + theme_bw() + 
+xlab(TeX("\\tilde{m}")) + ylab("time in minutes") 
